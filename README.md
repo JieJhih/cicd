c54cf2a3b5b956047d6b1d98be523054d8704a42
JieJhih Jhang <aawer12345tw@yahoo.com.tw>
2019-05-29 03:27:27 +0800

    add drone

--- README.md
+++
@@ -1,2 +0,0 @@
-# Simple flask app for cicd demo
-please follow the steps in this [blog](http://haktech.io/cicd-the-kubernetes-way/) .  
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func helloworld() string {
+	return "Hello test!!"
+}
+
+func main() {
+	fmt.Println(helloworld())
+}
