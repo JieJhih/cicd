0eda730dd34faceb6f8766adddd1dacd181360e2
JieJhih Jhang <aawer12345tw@yahoo.com.tw>
2019-05-29 20:39:48 +0800

    fix dockerfile

--- main_test.go
+++ main_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 // TestHelloWorld tests the func
 func TestHelloWorld(t *testing.T) {
-	if helloworld() != "Hello test!!" {
+	if helloworld() != "Hello code!!" {
 		t.Fatal("Test fail")
 	}
 }
