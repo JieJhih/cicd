3a38bdb32a8dad0ca42ec42fc001e6d6d5ef3ef5
JieJhih Jhang <aawer12345tw@yahoo.com.tw>
2019-05-29 23:23:18 +0800

    fix test

--- main_test.go
+++ main_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 // TestHelloWorld tests the func
 func TestHelloWorld(t *testing.T) {
-	if helloworld() != "Hello test!!" {
+	if helloworld() != "Hello helm!!" {
 		t.Fatal("Test fail")
 	}
 }
